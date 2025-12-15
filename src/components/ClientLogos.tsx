ca6833ea3b1c7df12df9225935c0c5bf29f38825
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2025-12-15 11:36:36 -0800

    max logo width

--- src/components/ClientLogos.tsx
+++ src/components/ClientLogos.tsx
@@ -32,7 +32,7 @@ const ClientLogos = ({ startAnimation = true }: ClientLogosProps) => {
             key={client.name}
             src={client.logo}
             alt={`${client.name} logo`}
-            className="h-3 md:h-6 w-auto object-contain grayscale brightness-0 dark:invert hover:opacity-100 transition-opacity duration-500"
+            className="h-3 md:h-6 w-auto max-w-[90px] md:max-w-none object-contain grayscale brightness-0 dark:invert hover:opacity-100 transition-opacity duration-500"
             style={{
               opacity: index < visibleCount ? 0.7 : 0,
             }}
