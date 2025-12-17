ea28d0f4f8974f3c4f6d03ed32438ab5982683d4
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2025-12-16 21:20:11 -0800

    arlo polish

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -201,7 +201,7 @@ const Home = () => {
                     ) : project.slug === 'arlo' && project.images.length >= 7 ? (
                       // Custom Arlo layout: left column 62% (image 1), right column 38% (images 4, 7)
                       // Second image in right column hidden on mobile
-                      <div className="flex flex-col md:flex-row gap-2 lg:gap-4 h-auto md:h-[30vh] lg:h-[40vh]">
+                      <div className="flex flex-col md:flex-row gap-2 lg:gap-4 lg:h-[45vh] md:h-[35vh]">
                         <div style={{ flex: '62 1 0%' }} className="overflow-hidden">
                           <ScrollImage
                             src={project.images[0].src}
