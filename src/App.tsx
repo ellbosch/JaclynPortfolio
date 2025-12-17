52f831a0202888dd459160f8aa051e14df597deb
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2025-12-17 14:58:03 -0800

    Replaced app with "Coming Soon"

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import PageLayout from './components/layout/PageLayout';
-import Home from './pages/Home';
-import ProjectPage from './pages/ProjectPage';
-import NotFound from './pages/NotFound';
-import { FilterProvider } from './context/FilterContext';
-import ScrollToTop from './components/ScrollToTop';
+import ComingSoon from './pages/ComingSoon';
 
 function App() {
-  return (
-    <FilterProvider>
-      <Router>
-        <ScrollToTop />
-        <Routes>
-          <Route element={<PageLayout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/project/:slug" element={<ProjectPage />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </Router>
-    </FilterProvider>
-  );
+  return <ComingSoon />;
 }
 
 export default App;
