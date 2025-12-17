c4834bfd0ad9fb5bca9d80be3fa225cfa11bccc4
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2025-12-16 20:30:18 -0800

    new logos

--- src/data/clients.ts
+++ src/data/clients.ts
@@ -8,6 +8,7 @@ export const clients: Client[] = [
   { name: 'Amazon', logo: '/images/logos/amazon.png' },
   { name: 'Samsung', logo: '/images/logos/samsung.png' },
   { name: 'Bose', logo: '/images/logos/bose.png' },
+  { name: 'Best Buy', logo: '/images/logos/bestbuy.png' },
   { name: 'Arlo', logo: '/images/logos/arlo.png' },
   { name: 'Netgear', logo: '/images/logos/netgear.png' },
   { name: 'Nice', logo: '/images/logos/nice.png' },
@@ -16,7 +17,8 @@ export const clients: Client[] = [
   { name: 'Jabra', logo: '/images/logos/jabra.png' },
   { name: 'Mode', logo: '/images/logos/mode.webp' },
   // { name: 'Whistle', logo: '/images/logos/whistle.jpg' },
-  { name: 'Modern Sprout', logo: '/images/logos/modern-sprout.png' },
+  { name: 'Modern Sprout', logo: '/images/logos/modernsprout.webp' },
   { name: 'Control4', logo: '/images/logos/control4.png' },
   { name: 'Nocturne', logo: '/images/logos/nocturne.webp' },
+  { name: 'Sports Research', logo: '/images/logos/sports-research.png' },
 ];
