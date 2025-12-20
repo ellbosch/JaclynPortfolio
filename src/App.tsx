fe9273a8806687905ee72142fd17ee84c06a0785
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2025-12-19 17:08:17 -0800

    Revert "Replaced app with "Coming Soon""
    This reverts commit 52f831a0202888dd459160f8aa051e14df597deb.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import ComingSoon from './pages/ComingSoon';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PageLayout from './components/layout/PageLayout';
+import Home from './pages/Home';
+import ProjectPage from './pages/ProjectPage';
+import NotFound from './pages/NotFound';
+import { FilterProvider } from './context/FilterContext';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
-  return <ComingSoon />;
+  return (
+    <FilterProvider>
+      <Router>
+        <ScrollToTop />
+        <Routes>
+          <Route element={<PageLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/project/:slug" element={<ProjectPage />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </Router>
+    </FilterProvider>
+  );
 }
 
 export default App;
